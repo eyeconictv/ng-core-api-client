3e999fb8ded480ff0406b21a3c492af0fbb4e355
Alex Deaconu <alex.deaconu@gmail.com>
2015-03-31 17:24:48 -0400

    [feature] Add client side array filter

--- src/js/svc-countries.js
+++ src/js/svc-countries.js
@@ -4,35 +4,37 @@
 
   angular.module("risevision.core.countries", ["risevision.common.gapi"])
 
-  .factory("getCoreCountries", ["coreAPILoader", "$q", "$log",
-      function (coreAPILoader, $q, $log) {
-        var deferred;
-        return function () {
-          if (deferred) {
-            return deferred.promise;
-          }
-          else {
-            deferred = $q.defer();  
-          }
-          
-          coreAPILoader().then(function (coreApi) {
-            return coreApi.country.list();
-          })
-          .then(function (resp) {
-            var items = resp.result;
-            if(!(items instanceof Array) && items.items) { items = items.items; }
-
-            deferred.resolve(items);
-          })
-          .then(null, function(e) {
-            $log.debug("getCoreCountries failed", e);
-            deferred.reject(e);
-            
-            deferred = null;
-          });
+  .factory("getCoreCountries", ["coreAPILoader", "$q", "$log", "$filter",
+    function (coreAPILoader, $q, $log, $filter) {
+      var deferred;
+      return function () {
+        if (deferred) {
           return deferred.promise;
-        };
-    }])
+        }
+        else {
+          deferred = $q.defer();  
+        }
+        
+        coreAPILoader().then(function (coreApi) {
+          return coreApi.country.list();
+        })
+        .then(function (resp) {
+          var items = resp.result ? resp.result.items : [];
+          if(items instanceof Array) { 
+            items = $filter("orderBy")(items, "name");
+          }
+
+          deferred.resolve(items);
+        })
+        .then(null, function(e) {
+          $log.debug("getCoreCountries failed", e);
+          deferred.reject(e);
+          
+          deferred = null;
+        });
+        return deferred.promise;
+      };
+  }])
   .factory("COUNTRIES", ["getCoreCountries", 
     function (getCoreCountries) {
       var countries = [];
